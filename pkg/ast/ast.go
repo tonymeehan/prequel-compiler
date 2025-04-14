b8d411e7db885420ea0340c3255da6f235731ad2
Sean Cunningham <sean@prequel.dev>
2025-04-14 16:04:30 -0400

    Add parent match id to diags

--- pkg/ast/ast.go
+++ pkg/ast/ast.go
@@ -428,13 +428,13 @@ func traverseTree(node *AstNodeT, wr io.Writer, depth int) error {
 
 	switch o := node.Object.(type) {
 	case *AstSeqMatcherT:
-		obj = fmt.Sprintf("%s.%d.%d w=%s pos_terms=%d neg_terms=%d scope=%s", node.Metadata.Type, node.Metadata.Depth, node.Metadata.MatchId, o.Window, len(o.Order), len(o.Negate), node.Metadata.Scope)
+		obj = fmt.Sprintf("%s.%d.%d.%d w=%s pos_terms=%d neg_terms=%d scope=%s", node.Metadata.Type, node.Metadata.Depth, node.Metadata.MatchId, node.Metadata.ParentMatchId, o.Window, len(o.Order), len(o.Negate), node.Metadata.Scope)
 	case *AstSetMatcherT:
-		obj = fmt.Sprintf("%s.%d.%d w=%s pos_terms=%d neg_terms=%d scope=%s", node.Metadata.Type, node.Metadata.Depth, node.Metadata.MatchId, o.Window, len(o.Match), len(o.Negate), node.Metadata.Scope)
+		obj = fmt.Sprintf("%s.%d.%d.%d w=%s pos_terms=%d neg_terms=%d scope=%s", node.Metadata.Type, node.Metadata.Depth, node.Metadata.MatchId, node.Metadata.ParentMatchId, o.Window, len(o.Match), len(o.Negate), node.Metadata.Scope)
 	case *AstLogMatcherT:
-		obj = fmt.Sprintf("%s.%d.%d w=%s pos_terms=%d neg_terms=%d scope=%s", node.Metadata.Type, node.Metadata.Depth, node.Metadata.MatchId, o.Window, len(o.Match), len(o.Negate), node.Metadata.Scope)
+		obj = fmt.Sprintf("%s.%d.%d.%d w=%s pos_terms=%d neg_terms=%d scope=%s", node.Metadata.Type, node.Metadata.Depth, node.Metadata.MatchId, node.Metadata.ParentMatchId, o.Window, len(o.Match), len(o.Negate), node.Metadata.Scope)
 	case *AstDescriptorT:
-		obj = fmt.Sprintf("%s.%d.%d scope=%s", node.Metadata.Type, node.Metadata.Depth, node.Metadata.MatchId, node.Metadata.Scope)
+		obj = fmt.Sprintf("%s.%d.%d.%d scope=%s", node.Metadata.Type, node.Metadata.Depth, node.Metadata.MatchId, node.Metadata.ParentMatchId, node.Metadata.Scope)
 	default:
 		return fmt.Errorf("unknown object type: %T", o)
 	}
