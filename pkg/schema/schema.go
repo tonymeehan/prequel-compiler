e2a74f1ecf1f6a1555423392d2d95688c2e7a544
Sean Cunningham <sean@prequel.dev>
2025-04-28 14:17:00 -0400

    Remove unused constants.

--- pkg/schema/schema.go
+++ pkg/schema/schema.go
@@ -1,15 +1,9 @@
 package schema
 
-import "errors"
-
 const (
 	ScopeOrganization = "organization"
 	ScopeCluster      = "cluster"
 	ScopeNode         = "node"
-)
-
-const (
-	EventTypeLog = "log"
 )
 
 type NodeTypeT string
@@ -25,37 +19,3 @@ const (
 func (t NodeTypeT) String() string {
 	return string(t)
 }
-
-const (
-	FieldId                   = "id"
-	FieldHash                 = "hash"
-	FieldSeverity             = "severity"
-	FieldGeneration           = "gen"
-	FieldKind                 = "kind"
-	FieldTags                 = "tags"
-	FieldMessage              = "message"
-	FieldName                 = "name"
-	FieldDisplayName          = "displayName"
-	FieldDescription          = "description"
-	FieldImageUrl             = "imageUrl"
-	FieldContainerName        = "containerName"
-	FieldContainerId          = "containerId"
-	FieldTimestamp            = "timestamp"
-	FieldK8sEventReason       = "reason"
-	FieldK8sEventType         = "type"
-	FieldK8sEventReasonDetail = "reasonDetail"
-)
-
-const (
-	KindRules      = "rules"
-	KindTags       = "tags"
-	KindCategories = "categories"
-)
-const (
-	PropFieldTimestamp = "timestamp"
-	PropFieldSpoolIdx  = "spool_idx"
-)
-
-var (
-	ErrField = errors.New("invalid field")
-)
