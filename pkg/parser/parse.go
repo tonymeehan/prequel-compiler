2b424a464d0a8864c0d24a0155214f850171af8b
Tony Meehan <19255656+tonymeehan@users.noreply.github.com>
2025-05-13 13:04:40 -0400

    Add scores (#15)
    Co-authored-by: Tony Meehan <tonymeehan@users.noreply.github.com>

--- pkg/parser/parse.go
+++ pkg/parser/parse.go
@@ -59,19 +59,21 @@ const (
 )
 
 type ParseCreT struct {
-	Id           string              `yaml:"id,omitempty" json:"id,omitempty"`
-	Severity     uint                `yaml:"severity" json:"severity"`
-	Title        string              `yaml:"title,omitempty" json:"title,omitempty"`
-	Category     string              `yaml:"category,omitempty" json:"category,omitempty"`
-	Tags         []string            `yaml:"tags,omitempty" json:"tags,omitempty"`
-	Author       string              `yaml:"author,omitempty" json:"author,omitempty"`
-	Description  string              `yaml:"description,omitempty" json:"description,omitempty"`
-	Impact       string              `yaml:"impact,omitempty" json:"impact,omitempty"`
-	Cause        string              `yaml:"cause,omitempty" json:"cause,omitempty"`
-	Mitigation   string              `yaml:"mitigation,omitempty" json:"mitigation,omitempty"`
-	References   []string            `yaml:"references,omitempty" json:"references,omitempty"`
-	Reports      uint                `yaml:"reports,omitempty" json:"reports,omitempty"`
-	Applications []ParseApplicationT `yaml:"applications,omitempty" json:"applications,omitempty"`
+	Id              string              `yaml:"id,omitempty" json:"id,omitempty"`
+	Severity        uint                `yaml:"severity" json:"severity"`
+	Title           string              `yaml:"title,omitempty" json:"title,omitempty"`
+	Category        string              `yaml:"category,omitempty" json:"category,omitempty"`
+	Tags            []string            `yaml:"tags,omitempty" json:"tags,omitempty"`
+	Author          string              `yaml:"author,omitempty" json:"author,omitempty"`
+	Description     string              `yaml:"description,omitempty" json:"description,omitempty"`
+	Impact          string              `yaml:"impact,omitempty" json:"impact,omitempty"`
+	ImpactScore     uint                `yaml:"impact_score,omitempty" json:"impact_score,omitempty"`
+	Cause           string              `yaml:"cause,omitempty" json:"cause,omitempty"`
+	Mitigation      string              `yaml:"mitigation,omitempty" json:"mitigation,omitempty"`
+	MitigationScore uint                `yaml:"mitigation_score,omitempty" json:"mitigation_score,omitempty"`
+	References      []string            `yaml:"references,omitempty" json:"references,omitempty"`
+	Reports         uint                `yaml:"reports,omitempty" json:"reports,omitempty"`
+	Applications    []ParseApplicationT `yaml:"applications,omitempty" json:"applications,omitempty"`
 }
 
 type ParseSequenceT struct {
